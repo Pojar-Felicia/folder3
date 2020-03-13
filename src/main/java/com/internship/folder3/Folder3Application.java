de93c004149bbd96e61da81ee94cf933b3f46215
Pojar-Felicia <pojarfelicia@gmail.com>
2020-03-13 15:02:51 +0200

    ComponentScan annotation

--- src/main/java/com/internship/folder3/Folder3Application.java
+++ src/main/java/com/internship/folder3/Folder3Application.java
@@ -7,8 +7,10 @@ import com.internship.folder3.controllers.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.internship.folder3","main"})
 public class Folder3Application {
 
     public static void main(String[] args) {
--- src/main/java/com/internship/folder3/controllers/ConstructorInjectedController.java
+++ src/main/java/com/internship/folder3/controllers/ConstructorInjectedController.java
@@ -1,7 +1,6 @@
 package com.internship.folder3.controllers;
 
 import com.internship.folder3.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
